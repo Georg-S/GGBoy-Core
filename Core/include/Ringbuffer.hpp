fe3842c827dce242471661ec56fca55d60c22372
Gork <g.steinhauser1@web.de>
2023-10-15 11:35:11 +0200

    Implement lock free Ringbuffer

---
+++ Core/include/Ringbuffer.hpp
@@ -0,0 +1,56 @@
+#pragma once
+#include <atomic>
+#include <memory>
+
+namespace ggb
+{
+	template<typename T, size_t MAX_SIZE>
+	class SingleProducerSingleConsumerRingbuffer
+	{
+	public:
+		bool push(const T& data)
+		{
+			return push(std::move(data));
+		}
+
+		bool push(T&& data)
+		{
+			auto writeIndex = m_lastWriteIndex.load();
+			auto readIndex = m_lastReadIndex.load();
+
+			writeIndex = ((writeIndex + 1) % MAX_SIZE);
+			if (writeIndex == readIndex)
+				return false;
+
+			m_buffer[writeIndex] = std::move(data);
+			m_lastWriteIndex.store(writeIndex);
+			return true;
+		}
+
+		T pop(T defaultReturnValue)
+		{
+			pop(&defaultReturnValue);
+			return defaultReturnValue;
+		}
+
+		bool pop(T* outValue)
+		{
+			auto writeIndex = m_lastWriteIndex.load();
+			auto readIndex = m_lastReadIndex.load();
+			if (writeIndex == readIndex)
+				return false;
+
+			readIndex = ((readIndex + 1) % MAX_SIZE);
+			const auto& result = m_buffer[readIndex];
+			m_lastReadIndex.store(writeIndex);
+
+			*outValue = result;
+			return true;
+		}
+
+	private:
+		std::atomic<size_t> m_lastReadIndex = 0;
+		std::atomic<size_t> m_lastWriteIndex = 0;
+		T m_buffer[MAX_SIZE] = {};
+	};
+}
