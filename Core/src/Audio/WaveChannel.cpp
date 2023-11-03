0c54a9e03faf1bfe75ad27303aa5b81a1214194d
Gork <g.steinhauser1@web.de>
2023-11-03 13:17:25 +0100

    Remove some magic numbers from channel3

--- Core/src/Audio/WaveChannel.cpp
+++ Core/src/Audio/WaveChannel.cpp
@@ -2,10 +2,10 @@
 
 #include "Utility.hpp"
 
-static constexpr uint16_t CHANNEL3_DAC_ENABLE_ADDRESS = 0xFF1A;
-static constexpr uint16_t CHANNEL3_PERIOD_LOW_ADDRESS = 0xFF1D;
-static constexpr uint16_t CHANNEL3_PERIOD_HIGH_AND_CONTROL_ADDRESS = 0xFF1E;
-static constexpr int SAMPLE_BUFFER_LENGTH = 32; // Wave RAM is 16 Bytes big and one byte holds two samples
+static constexpr int WAVE_CHANNEL_PERIOD_DIVIDER_FREQUENCY = 2097152;
+static constexpr int SAMPLES_PER_BYTE = 2;
+static constexpr int WAVE_RAM_SAMPLE_LENGTH = 32; // Wave RAM is 16 Bytes big and one byte holds two samples
+static constexpr int WAVE_RAM_MEMORY_SIZE = WAVE_RAM_SAMPLE_LENGTH / SAMPLES_PER_BYTE;
 
 ggb::WaveChannel::WaveChannel(BUS* bus)
 {
@@ -14,12 +14,12 @@ ggb::WaveChannel::WaveChannel(BUS* bus)
 
 void ggb::WaveChannel::setBus(BUS* bus)
 {
-	m_enabled = bus->getPointerIntoMemory(CHANNEL3_DAC_ENABLE_ADDRESS);
-	m_lengthTimer = bus->getPointerIntoMemory(0xFF1B);
-	m_outputLevel = bus->getPointerIntoMemory(0xFF1C);
-	m_periodLow = bus->getPointerIntoMemory(CHANNEL3_PERIOD_LOW_ADDRESS);
-	m_periodHighAndControl = bus->getPointerIntoMemory(CHANNEL3_PERIOD_HIGH_AND_CONTROL_ADDRESS);
-	m_waveRamStart = bus->getPointerIntoMemory(0xFF30);
+	m_enabled = bus->getPointerIntoMemory(AUDIO_CHANNEL_3_DAC_ENABLE_ADDRESS);
+	m_lengthTimer = bus->getPointerIntoMemory(AUDIO_CHANNEL_3_LENGTH_TIMER_ADDRESS);
+	m_outputLevel = bus->getPointerIntoMemory(AUDIO_CHANNEL_3_OUTPUT_LEVEL_ADDRESS);
+	m_periodLow = bus->getPointerIntoMemory(AUDIO_CHANNEL_3_PERIOD_LOW_ADDRESS);
+	m_periodHighAndControl = bus->getPointerIntoMemory(AUDIO_CHANNEL_3_PERIOD_HIGH_CONTROL_ADDRESS);
+	m_waveRamStart = bus->getPointerIntoMemory(AUDIO_CHANNEL_3_WAVE_PATTERN_RAM_START_ADDRESS);
 }
 
 void ggb::WaveChannel::step(int cyclesPassed)
@@ -33,27 +33,25 @@ void ggb::WaveChannel::step(int cyclesPassed)
 	if (m_periodCounter <= 0)
 	{
 		m_periodCounter = getPeriodCounter() + m_periodCounter;
-		m_sampleIndex = (m_sampleIndex + 1) % SAMPLE_BUFFER_LENGTH;
+		m_sampleIndex = (m_sampleIndex + 1) % WAVE_RAM_SAMPLE_LENGTH;
 	}
 }
 
 bool ggb::WaveChannel::write(uint16_t address, uint8_t value)
 {
-	if (address == CHANNEL3_DAC_ENABLE_ADDRESS) 
+	if (address == AUDIO_CHANNEL_3_DAC_ENABLE_ADDRESS)
 	{
 		*m_enabled = value;
 		m_isOn = isBitSet(*m_enabled, 7);
 		return true;
 	}
 
-	if (address == CHANNEL3_PERIOD_HIGH_AND_CONTROL_ADDRESS) 
+	if (address == AUDIO_CHANNEL_3_PERIOD_HIGH_CONTROL_ADDRESS)
 	{
 		*m_periodHighAndControl = value;
 		if (isBitSet(*m_periodHighAndControl, 7))
 			trigger();
 
-		//if (isBitSet(*m_periodHighAndControl, 6))
-		//	m_lengthCounter = getInitialLengthCounter();
 		return true;
 	}
 	return false;
@@ -61,7 +59,7 @@ bool ggb::WaveChannel::write(uint16_t address, uint8_t value)
 
 std::optional<uint8_t> ggb::WaveChannel::read(uint16_t address) const
 {
-	if (address == CHANNEL3_PERIOD_HIGH_AND_CONTROL_ADDRESS)
+	if (address == AUDIO_CHANNEL_3_PERIOD_HIGH_CONTROL_ADDRESS)
 		return *m_periodHighAndControl & 0b01000000;
 
 	return std::nullopt;
@@ -85,13 +83,13 @@ ggb::AUDIO_FORMAT ggb::WaveChannel::getSample() const
 		return 0;
 	const auto rightShift = outputLevel - 1;
 
-	const int waveRamIndex = m_sampleIndex / 2;
-	assert(waveRamIndex < (SAMPLE_BUFFER_LENGTH / 2));
+	const int waveRamIndex = m_sampleIndex / SAMPLES_PER_BYTE;
+	assert(waveRamIndex < WAVE_RAM_MEMORY_SIZE);
 	const auto twoSamples = m_waveRamStart[waveRamIndex];
 	uint16_t sample = 0;
 	// Two samples are stored in a one byte value of the wave RAM
 	// The upper nibble is the first sample and the lower nibble the second
-	if (m_sampleIndex % 2 == 0)
+	if (m_sampleIndex % SAMPLES_PER_BYTE == 0)
 		sample = twoSamples >> 4;
 	else
 		sample = twoSamples & 0b1111;
@@ -127,11 +125,12 @@ void ggb::WaveChannel::serialization(Serialization* serialization)
 
 int ggb::WaveChannel::getPeriodCounter() const
 {
+	constexpr auto CPU_CYCLES_PER_PERIOD_TICK = CPU_BASE_CLOCK / WAVE_CHANNEL_PERIOD_DIVIDER_FREQUENCY;
 	const uint16_t high = *m_periodHighAndControl & 0b111;
 	const uint16_t low = *m_periodLow;
 	const uint16_t num = (high << 8) | low;
 
-	return (2048 - num) * 2; // TODO
+	return (2048 - num) * CPU_CYCLES_PER_PERIOD_TICK; 
 }
 
 bool ggb::WaveChannel::isLengthShutdownEnabled() const
