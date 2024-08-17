4f80aef444d91bbc6b4bbf5b457ac326475d909a
Gork <g.steinhauser1@web.de>
2024-08-17 21:45:53 +0200

    Improve color correction

--- MainApp/src/Video.cpp
+++ MainApp/src/Video.cpp
@@ -20,15 +20,12 @@ SDLRenderer::~SDLRenderer()
 	SDL_DestroyTexture(m_texture);
 }
 
+// Color correction factors where found by bruteforcing the factors so that the result is somewhat similiar to BGB
 static ggb::RGBA colorCorrection(ggb::RGBA rgb)
 {
-	int r = rgb.r;
-	int g = rgb.g;
-	int b = rgb.b;
-
-	r = 3.0 / 4 * rgb.r + 1.0 / 4 * rgb.g;
-	g = 3.0 / 5 * rgb.g + 1.0 / 5 * rgb.b + 1.0 / 5 * rgb.r;
-	b = 4.0 / 6 * rgb.b + 1.0 / 6 * rgb.r + 1.0 / 6 * rgb.g;
+	int r = 0.9 * rgb.r + 0.1 * rgb.g + 0.1 * rgb.b;
+	int g = 0.2 * rgb.r + 0.6 * rgb.g + 0.3 * rgb.b;
+	int b = 0.1 * rgb.r + 0.1 * rgb.g + 0.9 * rgb.b;
 
 	rgb.r = std::clamp(r, 0, 255);
 	rgb.g = std::clamp(g, 0, 255);
